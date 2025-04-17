afb8dd4dfa0ed62869600f424c94182b8d4c8301
Anurag19-web <anuragnayak78910@gmail.com>
2025-04-17 10:17:56 +0530

    changes added

--- src/Task/TaskMain.jsx
+++ src/Task/TaskMain.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { TaskHome } from "./Home"
 import { Task } from "./Task"
 import { Result } from "./TaskResult"
+import { TaskHome } from "./TaskHome"
 
 export const TaskMain =()=>{
     const router = createBrowserRouter([
@@ -24,3 +24,29 @@ export const TaskMain =()=>{
         </>
     )
 }
+
+// import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// import { TaskHome } from "./Home"
+// import { Task } from "./Task"
+// import { Result } from "./TaskResult"
+
+// export const TaskMain = () => {
+//     const router = createBrowserRouter([
+//         {
+//             path: "/",
+//             element: <TaskHome />,
+//         },
+//         {
+//             path: "/questions",
+//             element: <Task />,
+//         },
+//         {
+//             path: "/result",
+//             element: <Result />
+//         }
+//     ]);
+
+//     return (
+//         <RouterProvider router={router} />
+//     );
+// };
